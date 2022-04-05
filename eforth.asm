0b1f481a88edb511bb6be534a600561c40170abf
phouchg <phouchg@protonmail.com>
2022-04-05 21:56:51 +0100

    more stack and arithmetic words

--- eforth.asm
+++ eforth.asm
@@ -364,6 +364,10 @@ NEXT1:	add rbp, CELLL		; pop loop index
 	$NEXT
 
 
+	;; Chapter 7 - Common Forth words
+
+	;; System variables
+
 	;; sp0 ( -- a )
 	;; Pointer to bottom of the data stack.
 	$CODE 3,'sp0',SZERO
@@ -457,11 +461,133 @@ NEXT1:	add rbp, CELLL		; pop loop index
 	push rax
 	$NEXT
 
+
+	;; More stack words
+
+	;; ?DUP ( w -- w w|0 )
+	;; Dup tos if it is not zero.
+	$CODE 4,'?DUP',QDUP
+	pop rax
+	or rax, rax 		; test tos
+	jz QDUP1
+	push rax
+QDUP1:	push rax		; push twice
+	$NEXT
+
+
+	;; ROT ( w1 w2 w3 -- w2 w3 w1 )
+	;; Rotate third item to top.
+	$CODE 3,'ROT',ROT
+	pop rax			; pop all 3 items
+	pop rbx
+	pop rcx
+	push rbx		; push them in the desired order
+	push rax
+	push rcx
+	$NEXT
+
+
+	;; 2DROP ( w w -- )
+	;; Discard top 2 items on the stack.
+	$CODE 5,'2DROP',DDROP
+	pop rax
+	pop rax
+	$NEXT
+
+
+	;; 2DUP ( w1 w2 -- w1 w2 w1 w2 )
+	;; Duplicate top two items.
+	$CODE 4,'2DUP',DDUP
+	pop rax			; pop w2
+	pop rbx			; pop w1
+	push rbx		; push w1
+	push rax		; push w2
+	push rbx		; push w1
+	push rax		; push w2
+	$NEXT
+
+
+	;; More arithmetic words
+
+	;; + ( w w -- sum )
+	;; Add top two items.
+	$CODE 1,'+',PLUS
+	pop rax
+	pop rbx
+	add rax, rbx		; add
+	push rax
+	$NEXT
+
+	;; D+ ( d d -- d )
+	;; Double integer addition.
+	$CODE 2,'D+',DPLUS
+	pop rax
+	pop rdx
+	pop rbx
+	pop rcx
+	add rdx, rcx		; add lower half
+	adc rax, rbx		; add upper half with carry
+	push rdx		; push lower sum
+	push rax		; push upper sum
+	$NEXT
+
+
+	;; NOT ( w -- w )
+	;; One's compliment of tos.
+	$CODE 3,'NOT',INVER
+	pop rax
+	not rax			; invert all bits
+	push rax
+	$NEXT
+
+
+	;; NEGATE ( n -- -n )
+	;; Two's compliment of tos.
+	$CODE 6,'NEGATE',NEGAT
+	pop rax
+	neg rax			; negate
+	push rax
+	$NEXT
+
+
+	;; DNEGATE ( d -- -d )
+	;; Two's compliment of top double integer.
+	$CODE 7,'DNEGATE',DNEGA
+	pop rax			; dh
+	pop rdx			; dl
+	neg rax			; negate dh
+	neg rdx			; negate dl
+	sbb rax, 0		; Carry into dh
+	push rdx
+	push rax
+	$NEXT
+
+
+	;; - ( n1 n2 -- n1-n2 )
+	;; Subtraction.
+	$CODE 1,'-',SUBBB
+	pop rbx
+	pop rax
+	sub rax, rbx		; sub
+	push rax
+	$NEXT
+
+
+	;; ABS ( n -- n )
+	;; Return the absolute value of n.
+	$CODE 3,'ABS',ABSS
+	pop rax
+	or rax, rax
+	jge ABS1
+	neg rax
+ABS1:	push rax
+	$NEXT
+
+
 	;; TEST COLON CALLS ( -- )
 	;; Test colon calls.
-	;; c b a b -> b a b c
 	$COLON 7,'TESTABC',TESTABC
-	dq DOLIT,99,DOLIT,98,DOLIT,97,DOLIT,10,UPLUS,DROP,OVER,DUPP,ZLESS,DROP,EMIT,EMIT,EMIT,EMIT,EXITT
+	dq DOLIT,100,DOLIT,197,SUBBB,ABSS,DOLIT,98,DOLIT,97,DOLIT,10,UPLUS,DROP,OVER,QDUP,ZLESS,DROP,EMIT,EMIT,EMIT,EMIT,EXITT
 
 
 	;; TEST ( -- )
